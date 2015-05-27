03a28f6c041886df626acda389bd8817e4b7ef73
merlin <tangrock@gmail.com>
2015-05-27 02:13:42 -0400

    test2

--- src/test/test4standard.scala
+++ src/test/test4standard.scala
@@ -1,14 +1,30 @@
 package test
 
+import quadtree.quadtree
 /**
  * Created by merlin on 5/26/15.
  */
 class test4standard {
 
     //add point
-
     //test the insert correct or not
 
-    //rang search
+  def test4insertPoint(Index:quadtree):Boolean=
+  {
+
+    false
+  }
+
+  //rang search
+  def test4rangeSearch(Index:quadtree):Boolean=
+  {
+
+    false
+  }
+
+  
+    //test quadtree iterator
+
+    //get the iterator
 
 }
